52a39d7b65fc868c486201f184640e27e5e91a86
Maho <madahzadeh18@gmail.com>
2019-05-25 21:37:54 +0300

    Word.cs  is done
    i't have all propertys and also asking time 's  first value code

--- Memocabulary/Memocabulary/Word.cs
+++ Memocabulary/Memocabulary/Word.cs
@@ -8,7 +8,22 @@ namespace Memocabulary
 {
     class Word
     {
+        public string EnlishName { get; set; }
+        public string TurkishName { get; set; }
+        public string WordKind { get; set; }//verb pronoun adjective noun adverb preposition conjunction...
+        public string ExampleSentence { get; set; } //opsionel
+        public DateTime WillAskDateTime { get; set; }//default is today affter asking it will be update
 
+        bool WordBorned = false;
+        Word()
+        {
+            if (WordBorned == false)
+            {
+                this.WillAskDateTime = DateTime.Now;
+                WordBorned = true;
+            }
+
+        }
 
     }
 }
